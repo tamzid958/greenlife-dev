8d882df8b0640a4ede3640e8a5d627d49abfb337
Tamzid Ahmed <tamjidahmed958@gmail.com>
2021-02-21 01:36:17 +0600

    driver added

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module greenlife-dev
 go 1.16
 
 require (
+	github.com/bdwilliams/go-jsonify v0.0.0-20141020182238-48749139e742 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/lib/pq v1.9.0 // indirect
 	golang.org/x/sys v0.0.0-20210219172841-57ea560cfca1 // indirect
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 	"log"
@@ -13,26 +12,41 @@ import (
 )
 
 type Donor struct {
-	Id      string `json:"id"`
-	Name    string `json:"name"`
-	Phone   string `json:"phone"`
-	Dob     string `json:"dob"`
-	Blood   string `json:"blood"`
-	Disease bool   `json:"disease"`
-	Rating  string `json:"rating"`
+	Id       string    `json:"id"`
+	Phone    string    `json:"phone"`
+	Dob      string    `json:"dob"`
+	Blood    string    `json:"blood"`
+	Disease  bool      `json:"disease"`
+	Location string    `json:"location"`
+	Rating   string    `json:"rating"`
+	Details  *Details  `json:"details"`
+	Donation *Donation `json:"donation"`
+}
+
+type Details struct {
+	FirstName string `json:"first_name"`
+	UserName  string `json:"username"`
+	Email     string `json:"email"`
+}
+
+type Donation struct {
+	Review          int    `json:"review"`
+	Status          bool   `json:"status"`
+	DonationDate    string `json:"donation_date"`
+	AppointmentDate string `json:"appointment_date"`
 }
 
 var _connStr = "postgres://postgres:12345@localhost/greenlife?sslmode=disable"
 
 var donors = []Donor{
-	{Id: "1", Name: "name", Phone: "0199999", Dob: "18 Jun 1982", Blood: "AB+", Disease: true, Rating: "4"},
-	{Id: "2", Name: "name", Phone: "0199999", Dob: "18 Jun 1982", Blood: "AB+", Disease: true, Rating: "4"},
-	{Id: "3", Name: "name", Phone: "0199999", Dob: "18 Jun 1982", Blood: "AB+", Disease: true, Rating: "4"},
-	{Id: "4", Name: "name", Phone: "0199999", Dob: "18 Jun 1982", Blood: "AB+", Disease: true, Rating: "4"},
-	{Id: "5", Name: "name", Phone: "0199999", Dob: "18 Jun 1982", Blood: "AB+", Disease: true, Rating: "4"},
-	{Id: "6", Name: "name", Phone: "0199999", Dob: "18 Jun 1982", Blood: "AB+", Disease: true, Rating: "4"},
-	{Id: "7", Name: "name", Phone: "0199999", Dob: "18 Jun 1982", Blood: "AB+", Disease: true, Rating: "4"},
-	{Id: "8", Name: "name", Phone: "0199999", Dob: "18 Jun 1982", Blood: "AB+", Disease: true, Rating: "4"},
+	{Id: "1", Phone: "0199999", Dob: "18 Jun 1982", Blood: "AB+", Disease: true, Location: "location", Rating: "4", Details: &Details{FirstName: "name", UserName: "user", Email: "Email"}},
+	{Id: "2", Phone: "0199999", Dob: "18 Jun 1982", Blood: "AB+", Disease: true, Location: "location", Rating: "4"},
+	{Id: "3", Phone: "0199999", Dob: "18 Jun 1982", Blood: "AB+", Disease: true, Location: "location", Rating: "4"},
+	{Id: "4", Phone: "0199999", Dob: "18 Jun 1982", Blood: "AB+", Disease: true, Location: "location", Rating: "4"},
+	{Id: "5", Phone: "0199999", Dob: "18 Jun 1982", Blood: "AB+", Disease: true, Location: "location", Rating: "4"},
+	{Id: "6", Phone: "0199999", Dob: "18 Jun 1982", Blood: "AB+", Disease: true, Location: "location", Rating: "4"},
+	{Id: "7", Phone: "0199999", Dob: "18 Jun 1982", Blood: "AB+", Disease: true, Location: "location", Rating: "4"},
+	{Id: "8", Phone: "0199999", Dob: "18 Jun 1982", Blood: "AB+", Disease: true, Location: "location", Rating: "4"},
 }
 
 func GetDonors(writer http.ResponseWriter, _ *http.Request) {
@@ -43,11 +57,12 @@ func GetDonors(writer http.ResponseWriter, _ *http.Request) {
 		log.Fatal(err)
 	}
 
-	rows, err := db.Query("SELECT * FROM PUBLIC.donor_donor ORDER BY id ASC")
-
-	fmt.Println(rows)
+	rows, err := db.Query("SELECT id, dob, phone, blood_group, disease, location FROM PUBLIC.donor_donor ORDER BY id ASC")
 
 	json.NewEncoder(writer).Encode(donors)
+
+	defer rows.Close()
+	defer db.Close()
 }
 func GetDonor(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "application/json")
@@ -100,7 +115,7 @@ func DeleteDonor(writer http.ResponseWriter, request *http.Request) {
 func main() {
 	r := mux.NewRouter()
 
-	donors = append(donors, Donor{Id: "9", Name: "name", Phone: "0199999", Dob: "18 Jun 1982", Blood: "AB+", Disease: true, Rating: "4"})
+	donors = append(donors, Donor{Id: "9", Phone: "0199999", Dob: "18 Jun 1982", Blood: "AB+", Disease: true, Location: "location", Rating: "4"})
 
 	r.HandleFunc("/dev/donors", GetDonors).Methods("GET")
 	r.HandleFunc("/dev/donors", CreateDonor).Methods("POST")
